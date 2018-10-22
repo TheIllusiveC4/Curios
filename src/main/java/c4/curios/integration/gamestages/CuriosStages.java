2260ccdb6646b017c07b8775e5e468421475a436
TheIllusiveC4 <itwasmec4@gmail.com>
2018-10-21 17:45:47 -0700

    some more game stages stuff

--- src/main/java/c4/curios/integration/gamestages/CuriosStages.java
+++ src/main/java/c4/curios/integration/gamestages/CuriosStages.java
@@ -1,18 +1,51 @@
 package c4.curios.integration.gamestages;
 
+import c4.curios.api.event.LivingCollectCuriosEvent;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import net.darkhax.gamestages.GameStageHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import javax.annotation.Nullable;
 import java.util.*;
 
 public class CuriosStages {
 
     private static final Map<String, Set<String>> slotToStages = Maps.newHashMap();
 
+    @SubscribeEvent
+    public void onCurioCollection(LivingCollectCuriosEvent evt) {
+
+        if (evt.getEntityLiving() instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer)evt.getEntityLiving();
+            Set<String> identifiers = evt.getIds();
+
+            for (Iterator<String> i = identifiers.iterator(); i.hasNext();) {
+                String id = i.next();
+
+                if (slotToStages.containsKey(id)) {
+                    Set<String> stages = getStages(id);
+
+                    if (stages != null && !GameStageHelper.hasAllOf(player, stages)) {
+                        i.remove();
+                    }
+                }
+            }
+        }
+    }
+
+    @Nullable
     public static Set<String> getStages(String identifier) {
-        return ImmutableSet.copyOf(slotToStages.getOrDefault(identifier, new HashSet<>()));
+        Set<String> stages = slotToStages.get(identifier);
+
+        if (stages != null) {
+            return ImmutableSet.copyOf(stages);
+        } else {
+            return null;
+        }
     }
 
     public static void addStageToSlot(String stage, String identifier) {
