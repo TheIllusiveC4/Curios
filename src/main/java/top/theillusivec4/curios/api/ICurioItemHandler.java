59e2d84c606ea21867f52f72cc147401ca34d16d
TheIllusiveC4 <itwasmec4@gmail.com>
2019-02-20 20:05:29 -0800

    some more javadocs

--- src/main/java/top/theillusivec4/curios/api/ICurioItemHandler.java
+++ src/main/java/top/theillusivec4/curios/api/ICurioItemHandler.java
@@ -9,17 +9,39 @@ import java.util.SortedMap;
 
 public interface ICurioItemHandler {
 
+    /**
+     * An immutable map of the current curios, sorted by the {@link CurioType} identifier
+     * @return  The current curios equipped
+     */
     ImmutableSortedMap<String, ItemStackHandler> getCurioMap();
 
+    /**
+     * An immutable map of the previous curios, sorted by the {@link CurioType} identifier
+     * This is only used to compare against {@link ICurioItemHandler#getCurioMap()} for content changes
+     * Note that any changes made to the {@link ItemStackHandler} objects, but not its contents, in
+     * {@link ICurioItemHandler#getCurioMap()} should be reflected in this map as well, otherwise syncing issues may occur
+     * @return  The previous curios equipped
+     */
     ImmutableSortedMap<String, ItemStackHandler> getPreviousCurioMap();
 
+    /**
+     * Sets the current curios map to the one passed in
+     * @param map   The curios collection that will replace the current one
+     */
     void setCurioMap(SortedMap<String, ItemStackHandler> map);
 
     /**
-    * Returns the total number of Curio slots across all identifiers
-    */
+     * The number of slots across all {@link CurioType} identifiers
+     * @return Number of slots
+     */
     int getSlots();
 
+    /**
+     * The {@link ItemStackHandler} associated with the {@link CurioType} identifier
+     *
+     * @param identifier
+     * @return
+     */
     ItemStackHandler getStackHandler(String identifier);
 
     ItemStack getStackInSlot(String identifier, int slot);
--- src/main/java/top/theillusivec4/curios/api/event/LivingCurioChangeEvent.java
+++ src/main/java/top/theillusivec4/curios/api/event/LivingCurioChangeEvent.java
@@ -2,10 +2,29 @@ package top.theillusivec4.curios.api.event;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.eventbus.api.Cancelable;
 
 import javax.annotation.Nonnull;
 
+/**
+ * {@link LivingCurioChangeEvent} is fired when the Curio of an EntityLivingBase changes. <br>
+ * This event is fired whenever changes in curios are detected in {@link net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent}. <br>
+ * This also includes entities joining the World, as well as being cloned. <br>
+ * This event is fired on server-side only. <br>
+ * <br>
+ * {@link #type} contains the affected {@link top.theillusivec4.curios.api.CurioType}. <br>
+ * {@link #from} contains the {@link ItemStack} that was equipped previously. <br>
+ * {@link #to} contains the {@link ItemStack} that is equipped now. <br>
+ * {@link #index} contains the index of the curio slot
+ * <br>
+ * This event is not {@link Cancelable}. <br>
+ * <br>
+ * This event does not have a result. {@link HasResult} <br>
+ * <br>
+ * This event is fired on the {@link MinecraftForge#EVENT_BUS}.
+ **/
 public class LivingCurioChangeEvent extends LivingEvent {
 
     private final String type;
