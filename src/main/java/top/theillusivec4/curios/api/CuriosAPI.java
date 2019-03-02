978ce6e8aa9cf5dc0b45ac6995e6e63f18f28e1a
TheIllusiveC4 <itwasmec4@gmail.com>
2019-03-01 23:22:25 -0800

    more robust getCurioEquipped API method

--- src/main/java/top/theillusivec4/curios/api/CuriosAPI.java
+++ src/main/java/top/theillusivec4/curios/api/CuriosAPI.java
@@ -22,6 +22,7 @@ package top.theillusivec4.curios.api;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Tuple;
 import net.minecraftforge.common.util.LazyOptional;
 import top.theillusivec4.curios.api.capability.CuriosCapability;
 import top.theillusivec4.curios.api.capability.ICurio;
@@ -29,6 +30,8 @@ import top.theillusivec4.curios.api.capability.ICurioItemHandler;
 import top.theillusivec4.curios.api.inventory.CurioStackHandler;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Set;
 
 public class CuriosAPI {
 
@@ -40,21 +43,40 @@ public class CuriosAPI {
         return entityLivingBase.getCapability(CuriosCapability.INVENTORY);
     }
 
-    public static int getCurioEquippedOfType(String identifier, Item item, final EntityLivingBase entityLivingBase) {
-        return getCuriosHandler(entityLivingBase).map(handler -> {
-            CurioStackHandler stackHandler = handler.getStackHandler(identifier);
+    /**
+     * Gets the first found ItemStack of the item type equipped in a curio slot, or null if no matches were found.
+     * @param item              The item to find
+     * @param entityLivingBase  The wearer of the item to be found
+     * @return  A data pair indicating the identifier of the curio slot and the slot index of the first found ItemStack
+     * matching the parameters. Null if no matches were found.
+     */
+    @Nullable
+    public static Tuple<String, Integer> getCurioEquipped(Item item, @Nonnull final EntityLivingBase entityLivingBase) {
+        Tuple<String, Integer> found = getCuriosHandler(entityLivingBase).map(handler -> {
+            Set<String> tags = CuriosRegistry.getCurioTags(item);
 
-            if (stackHandler != null) {
-                for (int i = 0; i < stackHandler.getSlots(); i++) {
-                    ItemStack stack1 = stackHandler.getStackInSlot(i);
+            for (String id : tags) {
+                CurioStackHandler stackHandler = handler.getStackHandler(id);
 
-                    if (!stack1.isEmpty() && item == stack1.getItem()) {
-                        return i;
+                if (stackHandler != null) {
+
+                    for (int i = 0; i < stackHandler.getSlots(); i++) {
+                        ItemStack stack = stackHandler.getStackInSlot(i);
+
+                        if (!stack.isEmpty() && item == stack.getItem()) {
+                            return new Tuple<>(id, i);
+                        }
                     }
                 }
             }
-            return -1;
-        }).orElse(-1);
+            return new Tuple<>("", 0);
+        }).orElse(new Tuple<>("", 0));
+
+        if (!found.getA().isEmpty()) {
+            return found;
+        } else {
+            return null;
+        }
     }
 
     public static void setTypeEnabled(String id, boolean enabled) {
