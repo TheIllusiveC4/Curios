8b027a77acae2944ab55a36a907e72c95bcdd9e4
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2022-06-20 19:30:40 -0700

    fix commands not loading correctly when used in datapack functions, closes #240

--- src/main/java/top/theillusivec4/curios/server/command/CurioArgumentType.java
+++ src/main/java/top/theillusivec4/curios/server/command/CurioArgumentType.java
@@ -34,6 +34,7 @@ import java.util.concurrent.CompletableFuture;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.network.chat.TranslatableComponent;
+import top.theillusivec4.curios.api.CuriosApi;
 
 public class CurioArgumentType implements ArgumentType<String> {
 
@@ -66,7 +67,7 @@ public class CurioArgumentType implements ArgumentType<String> {
   public String parse(StringReader reader) throws CommandSyntaxException {
     String s = reader.readUnquotedString();
 
-    if (!slotIds.contains(s)) {
+    if (CuriosApi.getSlotHelper() != null && !slotIds.contains(s)) {
       throw UNKNOWN_TYPE.create(s);
     } else {
       return s;
