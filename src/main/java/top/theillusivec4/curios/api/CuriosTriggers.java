58339c7bc4456e97b85d3f71af2dae432316aa72
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2024-01-19 23:38:41 -0800

    Return empty instance

--- src/main/java/top/theillusivec4/curios/api/CuriosTriggers.java
+++ src/main/java/top/theillusivec4/curios/api/CuriosTriggers.java
@@ -2,21 +2,31 @@ package top.theillusivec4.curios.api;
 
 import javax.annotation.Nonnull;
 import net.minecraft.advancements.CriterionTriggerInstance;
+import net.minecraft.advancements.critereon.AbstractCriterionTriggerInstance;
+import net.minecraft.advancements.critereon.ContextAwarePredicate;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.advancements.critereon.LocationPredicate;
+import net.minecraft.resources.ResourceLocation;
 
 public final class CuriosTriggers {
 
   @Nonnull
   public static CriterionTriggerInstance equip(ItemPredicate.Builder itemPredicate) {
     CuriosApi.apiError();
-    return null;
+    return new EmptyInstance();
   }
 
   @Nonnull
   public static CriterionTriggerInstance equipAtLocation(ItemPredicate.Builder itemPredicate,
                                                          LocationPredicate.Builder locationPredicate) {
     CuriosApi.apiError();
-    return null;
+    return new EmptyInstance();
+  }
+
+  private static final class EmptyInstance extends AbstractCriterionTriggerInstance {
+
+    public EmptyInstance() {
+      super(new ResourceLocation(CuriosApi.MODID, "empty"), ContextAwarePredicate.ANY);
+    }
   }
 }
