89d3d17b5d0b798281b532310acd79fc3da9d874
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2024-02-27 04:43:26 -0800

    Fix missing service provider on Forge, closes #381

---
+++ forge/src/main/java/top/theillusivec4/curios/platform/ForgeCurios.java
@@ -0,0 +1,25 @@
+package top.theillusivec4.curios.platform;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.platform.services.ICuriosPlatform;
+
+public class ForgeCurios implements ICuriosPlatform {
+
+  @Override
+  public boolean makesPiglinsNeutral(ItemStack stack, LivingEntity livingEntity) {
+    return stack.makesPiglinsNeutral(livingEntity);
+  }
+
+  @Override
+  public boolean canWalkOnPowderedSnow(ItemStack stack, LivingEntity livingEntity) {
+    return stack.canWalkOnPowderedSnow(livingEntity);
+  }
+
+  @Override
+  public boolean isEnderMask(ItemStack stack, Player player, EnderMan enderMan) {
+    return stack.isEnderMask(player, enderMan);
+  }
+}
