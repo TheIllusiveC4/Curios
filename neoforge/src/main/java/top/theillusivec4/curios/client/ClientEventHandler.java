85dabd724b6df1184e97f6bf4d3a745b45ed1f20
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2025-03-24 21:55:13 -0700

    Restructure tooltips for NeoForge conventions, closes #496

--- neoforge/src/main/java/top/theillusivec4/curios/client/ClientEventHandler.java
+++ neoforge/src/main/java/top/theillusivec4/curios/client/ClientEventHandler.java
@@ -23,7 +23,6 @@ package top.theillusivec4.curios.client;
 import com.google.common.collect.Multimap;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
@@ -38,14 +37,16 @@ import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.neoforge.client.event.AddAttributeTooltipsEvent;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.GatherSkippedAttributeTooltipsEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.common.util.AttributeTooltipContext;
 import net.neoforged.neoforge.common.util.AttributeUtil;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotContext;
-import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.common.network.client.CPacketOpenCurios;
 
 public class ClientEventHandler {
@@ -59,21 +60,56 @@ public class ClientEventHandler {
   }
 
   @SubscribeEvent
-  public void onTooltip(ItemTooltipEvent evt) {
+  public void onAttributeTooltip(final AddAttributeTooltipsEvent evt) {
+    AttributeTooltipContext context = evt.getContext();
+    ItemStack stack = evt.getStack();
+    GatherSkippedAttributeTooltipsEvent skipped =
+        NeoForge.EVENT_BUS.post(new GatherSkippedAttributeTooltipsEvent(stack, context));
+
+    if (skipped.isSkippingAll()) {
+      return;
+    }
+    List<Component> attributesTooltip = new ArrayList<>();
+    Player player = context.player();
+    List<String> slots = getItemStackSlots(stack, player);
+
+    for (String identifier : slots) {
+      SlotContext slotContext = new SlotContext(identifier, player, 0, false, true);
+      Multimap<Holder<Attribute>, AttributeModifier> attributes =
+          CuriosApi.getAttributeModifiers(slotContext, CuriosApi.getSlotId(slotContext), stack);
+      attributes.values().removeIf(modifier -> skipped.isSkipped(modifier.id()));
+
+      if (attributes.isEmpty()) {
+        continue;
+      }
+      attributesTooltip.add(Component.empty());
+      attributesTooltip.add(
+          Component.translatable("curios.modifiers." + identifier).withStyle(ChatFormatting.GOLD));
+
+      if (player != null) {
+        AttributeUtil.applyTextFor(
+            stack,
+            attributesTooltip::add,
+            attributes,
+            AttributeTooltipContext.of(player, context, context.flag()));
+      }
+    }
+    evt.addTooltipLines(
+        CuriosApi.getCurio(stack)
+            .map(curio -> curio.getAttributesTooltip(attributesTooltip, context))
+            .orElse(attributesTooltip)
+            .toArray(new Component[0]));
+  }
+
+  @SubscribeEvent
+  public void onTooltip(final ItemTooltipEvent evt) {
     ItemStack stack = evt.getItemStack();
     Player player = evt.getEntity();
 
     if (stack.isEmpty()) {
       return;
     }
-    List<Component> tooltip = evt.getToolTip();
-    Set<String> tags = Set.copyOf((player != null ? CuriosApi.getItemStackSlots(stack, player) :
-        CuriosApi.getItemStackSlots(stack, FMLLoader.getDist() == Dist.CLIENT)).keySet());
-
-    if (tags.contains("curio")) {
-      tags = Set.of("curio");
-    }
-    List<String> slots = new ArrayList<>(tags);
+    List<String> slots = getItemStackSlots(stack, player);
 
     if (slots.isEmpty()) {
       return;
@@ -92,31 +128,26 @@ public class ClientEventHandler {
       slotsTooltip.append(type);
     }
     Item.TooltipContext context = evt.getContext();
-    Optional<ICurio> curio = CuriosApi.getCurio(stack);
-    tooltip.addAll(1,
-        curio.isPresent() ? curio.get().getSlotsTooltip(List.of(slotsTooltip), context) :
-            List.of(slotsTooltip));
-    List<Component> attributesTooltip = new ArrayList<>();
+    List<Component> toAdd = List.of(slotsTooltip);
+    evt.getToolTip()
+        .addAll(
+            1,
+            CuriosApi.getCurio(stack)
+                .map(curio -> curio.getSlotsTooltip(toAdd, context))
+                .orElse(toAdd));
+  }
 
-    for (String identifier : slots) {
-      SlotContext slotContext = new SlotContext(identifier, player, 0, false, true);
-      Multimap<Holder<Attribute>, AttributeModifier> attributes =
-          CuriosApi.getAttributeModifiers(slotContext, CuriosApi.getSlotId(slotContext), stack);
+  private static List<String> getItemStackSlots(ItemStack stack, Player player) {
+    Set<String> slots =
+        Set.copyOf(
+            (player != null
+                    ? CuriosApi.getItemStackSlots(stack, player)
+                    : CuriosApi.getItemStackSlots(stack, FMLLoader.getDist() == Dist.CLIENT))
+                .keySet());
 
-      if (attributes.isEmpty()) {
-        continue;
-      }
-      attributesTooltip.add(Component.empty());
-      attributesTooltip.add(
-          Component.translatable("curios.modifiers." + identifier).withStyle(ChatFormatting.GOLD));
-
-      if (player != null) {
-        AttributeUtil.applyTextFor(stack, attributesTooltip::add, attributes,
-            AttributeTooltipContext.of(player, context, evt.getFlags()));
-      }
+    if (slots.contains("curio")) {
+      slots = Set.of("curio");
     }
-    tooltip.addAll(2,
-        curio.isPresent() ? curio.get().getAttributesTooltip(attributesTooltip, context) :
-            attributesTooltip);
+    return new ArrayList<>(slots);
   }
 }
